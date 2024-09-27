908bc8661f5aae534d828da0cd3a8951dbadf56b
Sergei Fladung <sergeifladung@web.de>
2024-09-28 00:25:29 +0200

    Preliminary model for Product created.

---
+++ ProductListAnalyzer/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace ProductListAnalyzer.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        // mandatory field
+        public string BrandName { get; set; } = string.Empty;
+
+        // mandatory field
+        public string PricePerUnitText { get; set; } = string.Empty;
+
+        // nullable, as not mandatory
+        public string? descriptionText { get; set; }
+    }
+}
