5bbd9b1d939e5889a1de51d0cc9ed7b10339528f
Sergei Fladung <sergeifladung@web.de>
2024-09-28 00:12:48 +0200

    Preliminary model for article created.

---
+++ ProductListAnalyzer/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿namespace ProductListAnalyzer.Models
+{
+    public class Article
+    {
+        public int Id { get; set; }
+
+        // nullable, as not mandatory
+        public string? ShortDescription { get; set; }
+
+        public double Price { get; set; } 
+
+        // mandatory field
+        public string Unit { get; set; } = string.Empty;
+
+        // mandatory field
+        public string PricePerUnitText { get; set; } = string.Empty;
+
+        // nullable, as not mandatory
+        public string? Image { get; set; }
+    }
+}
