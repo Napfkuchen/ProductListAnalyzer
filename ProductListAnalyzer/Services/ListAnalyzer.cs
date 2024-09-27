f671aee6ae3f119b6e5516686cc75fbb5dcb9adf
Sergei Fladung <sergeifladung@web.de>
2024-09-28 01:08:34 +0200

    Creat C# class "ListAnalyzer" for the actual functional sorting and filtering action, including the GetArticlesWithPriceAndSortByUnitPrice method to return articles with a price of 17.99, implemented as a variable input parameter instead of fixed amount.

---
+++ ProductListAnalyzer/Services/ListAnalyzer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.TagHelpers;
+using ProductListAnalyzer.Models;
+using System.Text.RegularExpressions;
+
+namespace ProductListAnalyzer.Services
+{
+    public class ListAnalyzer
+    {
+        // Returns Items with a price of €17.99 sorted by ascending unit price 
+        public List<Article> GetArticlesByPriceAndSortByUnitPrice(List<Article> articles, double targetPrice) { 
+            return articles
+                .Where(a => a.Price == targetPrice)
+                .OrderBy(a => a.PricePerUnitText)
+                .ToList();
+        }
+    }
+}
