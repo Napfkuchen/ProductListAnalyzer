1534cf1d236f3ad4559bf94fa03e5fa45b0269fd
Sergei Fladung <sergeifladung@web.de>
2024-09-28 20:59:39 +0200

    Creat class ProductData.cs for clearer differentiation between technical and functional logic.

---
+++ ProductListAnalyzer/Models/ProductData.cs
@@ -0,0 +1,12 @@
+﻿namespace ProductListAnalyzer.Models
+{
+    public class ProductData
+    {
+        public List<Article> Articles { get; set; } = new List<Article>();
+
+        // load Data from JSON-File by given path
+        public void LoadFromJson(string pathToJsonFile) {
+
+        }
+    }
+}
