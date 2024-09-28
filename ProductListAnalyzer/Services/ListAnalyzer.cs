0c25d2b03a191d3f1120b08fa2a2e5547354c348
Sergei Fladung <sergeifladung@web.de>
2024-09-28 21:25:03 +0200

    Added a few mock-methods to ListAnalyzer for the required functions/routes, extracted sorting algorithm from GetByPriceAndSortByUnitPrice-Method into own method and some minor cosmetic correction.

--- ProductListAnalyzer/Services/ListAnalyzer.cs
+++ ProductListAnalyzer/Services/ListAnalyzer.cs
@@ -6,9 +6,32 @@ namespace ProductListAnalyzer.Services
     public class ListAnalyzer
     {
         // Returns Items with a price of €17.99 sorted by ascending unit price 
-        public List<Article> GetArticlesByPriceAndSortByUnitPrice(List<Article> articles, double targetPrice) { 
-            return articles
+        public List<Article> GetByPriceAndSortByUnitPrice(List<Article> articles, double targetPrice) {
+            return sortByUnitPrice(articles)
                 .Where(a => a.Price == targetPrice)
+                .ToList();
+        }
+
+        // Returns most expensive item(s) 
+        public List<Article> GetMostExpensive(List<Article> articles) {
+            return null;
+        }
+
+        // Returns cheapest item(s) 
+        public List<Article> GetCheapest(List<Article> articles)
+        {
+            return null;
+        }
+
+        // Returns list with all three main Listing/sorting requirements
+        public List<Article> doAll(List<Article> articles) {
+            return null;
+        }
+
+        // Returns Items with a price of €17.99 sorted by ascending unit price 
+        public List<Article> sortByUnitPrice(List<Article> articles)
+        {
+            return articles
                 .OrderBy(a => a.PricePerUnitText)
                 .ToList();
         }
