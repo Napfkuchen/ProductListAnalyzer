c373bee2cb3eda1be684b39a7735ba9ae5d0d55c
Sergei Fladung <sergeifladung@web.de>
2024-09-30 00:44:02 +0200

    Added few changes regarding logging in appsettings and finally set up APIController.cs for some first successfull startrunnings. Still seems to be an error in ListAnalyzer class, at least for MostBottles, but perhaps, for all methods.

--- ProductListAnalyzer/Controllers/APIController.cs
+++ ProductListAnalyzer/Controllers/APIController.cs
@@ -6,12 +6,14 @@ using System.Net.Http;
 
 namespace ProductListAnalyzer.Controllers {
     [ApiController]
-    [Route("api/[controller]")]
+    [Route("API")]
     public class APIController : ControllerBase {
         private readonly ListAnalyzer _listAnalyzer;
+        private readonly ILogger<APIController> _logger;
 
-        public APIController(ListAnalyzer listanAnalyzer) {
+        public APIController(ListAnalyzer listanAnalyzer, ILogger<APIController> logger) {
             _listAnalyzer = listanAnalyzer;
+            _logger = logger;
         }
 
         // Route for returning list of most expensive and cheapest articles with "/api/APIController/getMostExpensive"
@@ -42,11 +44,18 @@ namespace ProductListAnalyzer.Controllers {
         // Route list of articles with most bottles with "/api/APIController/getMostBottles"
         [HttpGet("mostBottles")]
         public IActionResult GetMostBottles(string url) {
-            var jsonContent = FetchJsonFromUrl(url);
-            var articles = JsonConvert.DeserializeObject<List<Article>>(jsonContent);
-            var mostBottles = _listAnalyzer.GetMostBottles(articles);
+            try {
+                var jsonContent = FetchJsonFromUrl(url);
+                var articles = JsonConvert.DeserializeObject<List<Article>>(jsonContent);
+                var mostBottles = _listAnalyzer.GetMostBottles(articles);
+                _logger.LogInformation($"Most bottles result: {JsonConvert.SerializeObject(mostBottles)}");
 
-            return Ok(mostBottles);
+                return Ok(mostBottles);
+            }
+            catch (Exception ex) {
+                _logger.LogError($"Error: {ex.Message}");
+                return StatusCode(500, "Error fetching data");
+            }
         }
 
         // Route for returning list of articles including results of routs of all other routes with "/api/APIController/doAllRoutes"
